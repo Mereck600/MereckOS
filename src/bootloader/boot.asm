b0370dc35f3bf21048cf2ee83b008caa963192d0
Mereck600 <mereckmcg@gmail.com>
2025-04-06 01:01:20 -0400

    start of bootloader and kernel

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -16,6 +16,36 @@ bits 16 ; emit 16bit code so it can be backwards compatible start on 16 bit and 
 
 %define ENDL 0x0D,0x0A ;setting macro for endline 
 
+
+
+#
+# FAT12 header
+#
+jmp short start
+no op
+
+bdb_oem:	db 'MSWIN4.1' ;8 bytes
+bdb_bytes_per_sector: dw 512
+bdb_sectors_per_cluster: db 1
+bdb_reserved_sectors: dw 1
+bdb_fat_count:	db 2
+bdb_dir_entries_count: dw 0E0h 
+bdb_total_sectors: dw 2880  ; 288-*512 =1.44mb
+bdb_media_descriptor_type: db 0F0h ; F0 = 3.5*floppy disk
+bdb_sectors_per_fat: dw 9 ;9 sectors/fat
+bdb_heads: 	dw 2
+bdb_hidden_sectors: dd 0
+bdb_large_sector_count: dd 0
+
+# extended boot sector
+ebr_drive_number: 	db 0
+					db 0 ;reserved
+ebr_signature: 		db 29h 
+ebr_volume_id:		db 12h, 34h, 56h, 78h ;serial num
+ebr_volume_label: 	db 'MERECK OS O' ;;11 bytes padded with spaces
+
+
+
 start:
 	jmp main ;since func above main we need to jump to main
 
@@ -34,7 +64,7 @@ puts:
 	jz .done
 	
 
-	mov ah,0x0e
+	mov ah,0x0e ; moves from source to destination
 	mov bh,0
 	int 0x10
 	jmp .loop	
